eaa15847556aa0964e4aa1266f1eec4b0b2eddae
Chuckbibi <chuckbibi58@gmail.com>
2018-02-24 23:31:24 +0800

    处理了filecache.py中函数在运行中会引发的异常，并单独给cachetools文件夹创建了一个utils模块

--- scripts/python/CacheTools/utils/filecache.py
+++ scripts/python/CacheTools/utils/filecache.py
@@ -5,6 +5,16 @@ import os
 from shutil import rmtree
 
 
+def check_hasUnsaved():
+
+    if hou.hipFile.hasUnsavedChanges():
+        if hou.ui.displayMessage(
+            "The current hip isn't saved ,you needs to be saved.",
+            buttons = (" Cancel ",)
+            ) == 0:
+            exit()
+
+
 def open_path(node):
 
     try:
@@ -14,22 +24,30 @@ def open_path(node):
 
     except WindowsError:
         if hou.ui.displayMessage(
-            "Geometry file path cannot be empty! Do you need to create one?", ("Yes！","Next Time！")
+            "Geometry file path cannot be empty! Do you need to create one?",
+            buttons = (" Yes ", " Next Time ")
             ) == 0:
 
-            os.makedirs(path_dir)
-            os.startfile(path_dir)
+            try:
+                os.makedirs(path_dir)
+                os.startfile(path_dir)
+
+            except WindowsError:
+                check_hasUnsaved()
+                os.makedirs(path_dir)
+                os.startfile(path_dir)
+
+        else:
+            exit()
 
 
-def destroy_geo(node):
-
-    path_parm = node.evalParm("file")
-    path_dir = os.path.dirname(path_parm) + "/"
+def generate_files_list(node, file):
+    
+    path_parm = file
     path_file = path_parm.split(".")
     path_files = []
 
     for i in range(len(path_file)):
-
         if path_file[i].isdigit():        
             delimiter = "."
             frame_end_parm = node.evalParmTuple("f")
@@ -39,38 +57,44 @@ def destroy_geo(node):
                 start_num = "%04d" % (j+1)
                 path_file[i] = start_num
                 path_files.append(delimiter.join(path_file))
+    
+    return path_files
 
+
+def destroy_geo(node):
+
+    path_parm = node.evalParm("file")
+    path_dir = os.path.dirname(path_parm) + "/"
+    path_files = generate_files_list(node, path_parm)
 
     user = hou.ui.displayMessage(
-            "Destroy all cache under current folder?\nor Current Geometry file cache?", ("All", "Current", "Cancel")
+            "Destroy all cache under current folder?\nor Current Geometry file cache?",
+            buttons = (" All ", " Current ", " Cancel ")
             )
 
     if user  == 0:
-
         try:
             shutil.rmtree(path_dir)
+            hou.ui.setStatusMessage("Folder destroy done!")
 
         except WindowsError:
             if hou.ui.displayMessage(
                 "Folder does not exist!!!", ("Cancel",)
                 ) == 0:
-
                 exit()
     
     elif user == 1: 
-
         try:
             for i in range(len(path_files)):
                 os.remove(path_files[i])
-
+                hou.ui.setStatusMessage("Current cache destroy done!")
+                
         except WindowsError:
             if hou.ui.displayMessage(
                 "File does not exist!!!", ("Cancel",)
                 ) == 0:
-
                 exit()
 
     elif user == 2:
-
         exit()
 
